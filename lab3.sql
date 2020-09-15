5516febbb8b845c92c96ac23fd38902a633809b4
michaelwise12 <Michael.Wise1@edu>
2020-09-15 17:07:48 -0400

    Completed questions 1 & 2 for Lab #3.

---
+++ lab3.sql
@@ -0,0 +1,23 @@
+------------------------------------------------------------------------------------------------------
+-- Author: Michael Wise
+--
+-- Date: 17 September 2020
+--
+-- Class: CMPT308 Database Management
+--
+-- Professor: Dr. Alan Labouseur
+-- 
+-- Lab #3: Getting Started with SQL Queries
+------------------------------------------------------------------------------------------------------
+
+-- Question 1: List	the	order number and total dollars of all orders. --
+select orderNum, totalUsd
+from Orders;
+
+-- Question 2: List the last name and home city of people whose prefix is "Dr.". --
+select lastName, homeCity
+from People
+where prefix = 'Dr.';
+
+
+
