55e58ce164906c5ffa5993586a198f6edd1ed5ad
michaelwise12 <Michael.Wise1@edu>
2020-09-15 17:24:40 -0400

    Added solutions to question 3 and 4 for Lab #3.

--- lab3.sql
+++ lab3.sql
@@ -11,7 +11,7 @@
 ------------------------------------------------------------------------------------------------------
 
 -- Question 1: List the order number and total dollars of all orders. --
-select orderNum, totalUsd
+select orderNum, totalUSD
 from Orders;
 
 -- Question 2: List the last name and home city of people whose prefix is "Dr.". --
@@ -19,5 +19,15 @@ select lastName, homeCity
 from People
 where prefix = 'Dr.';
 
+-- Question 3: List id, name, and price of products with quantity more than 1007. --
+select prodId, name, priceUSD
+from Products
+where qtyOnHand > 1007;
+
+-- Question 4: List the first name and home city of people born in the 1950s. --
+select firstName, homeCity
+from People
+where DOB >= '1950-01-01'
+  and DOB <  '1960-01-01';
 
 
