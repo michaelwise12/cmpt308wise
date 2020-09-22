2d341bd6a8c879074e13545b73a82d2696f8d310
michaelwise12 <Michael.Wise1@edu>
2020-09-22 17:30:23 -0400

    Added queries for #9 & and #10.

--- lab4.sql
+++ lab4.sql
@@ -49,7 +49,7 @@ from Orders
 where prodId in ('p01', 'p07');
 			 
 -- Question #6: Get the ID of customers who ordered both products 'p01' and 'p07'. List 
--- the IDs in order from lowest to highest. Include each ID only once. --
+-- the IDs in order from highest to lowest. Include each ID only once. --
 select distinct custId
 from Orders
 where prodId = 'p01'
@@ -57,7 +57,7 @@ where prodId = 'p01'
 				   from Orders
 				   where prodId = 'p07'
 				  )
-order by custId ASC;
+order by custId DESC;
 			 
 -- Question #7: Get the first and last names of agents who sold products 'p05' or 'p07'
 -- in order by last name from Z to A. --
@@ -85,3 +85,31 @@ where pid in (select pid
 			 )
 order by homeCity ASC;
 
+-- Question #9: Get the unique ids of products ordered through any agent who takes at least one
+-- order from a customer in Toronto, sorted by id from highest to lowest. (This is not the same
+-- as asking for ids of products ordered by customers in Toronto.) --
+
+select distinct prodId
+from Orders
+where agentId in (select distinct agentId -- in this snapshot, the applicable agents have ids 2 & 3
+				  from Orders
+				  where custId in (select pid
+				 				   from People
+				 				   where homeCity = 'Toronto')
+				 )
+order by prodId DESC;
+
+-- Question #10: Get the last name and the home city for all customers who place orders
+-- through agents in Teaneck or Santa Monica. --
+select lastName, homeCity
+from People
+where pid in (select distinct custId -- in this snapshot, customers 4 & 8 place orders through 5
+			  from Orders
+			  where agentId in (select pid   -- in this snapshot, they are agents with id 5 & 6
+								from Agents
+								where pid in (select pid
+											  from People
+											  where homeCity in ('Teaneck', 'Santa Monica')
+											 )
+							   )
+			 );
