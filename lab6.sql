3244d6387b1154007d56af3a534c3dad0f67ecfa
michaelwise12 <Michael.Wise1@edu>
2020-10-12 23:20:04 -0400

    Added queries #3 and #4. Pain level: moderate.

--- lab6.sql
+++ lab6.sql
@@ -15,13 +15,29 @@ select city
 from Products
 group by city
 order by count(*) DESC
-limit 3;
+limit 2;
 
 -- Question #2: Display the names of products whose priceUSD is at or above the average
 -- priceUSD, in reverse-alphabetical order. --
-select *
+select name
 from Products
 where priceUSD >= (select avg(priceUSD)
                    from Products
                   )
 order by name DESC; -- In this snapshot, a brilliant Red Barchetta significantly affects the average.
+
+-- Question #3: Display the customer last name, product id ordered, and the totalUSD for all
+-- orders made in March, sorted by totalUSD from high to low. --
+select p.lastName, o.prodId, o.totalUSD
+from People p inner join Customers c on p.pid = c.pid
+              inner join Orders o    on c.pid = o.custId
+where extract(month from o.dateOrdered) = 3
+order by totalUSD DESC;
+
+-- Question #4: Display the last name of all customers (in reverse alphabetical order) and their
+-- total ordered, and nothing more. (Hint: use coalesce to avoid showing NULLs.) --
+select p.lastName, coalesce(sum(o.totalUSD), 0)
+from People p inner join      Customers c on p.pid = c.pid
+              left outer join Orders o    on c.pid = o.custId
+group by p.lastName
+order by p.lastName DESC;
