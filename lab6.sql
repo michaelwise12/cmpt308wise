3ef0e97b47c760353073ad140f8ad4bb6d9b3f52
michaelwise12 <Michael.Wise1@edu>
2020-10-13 16:02:34 -0400

    Added query #6 with its corresponding explanation.

--- lab6.sql
+++ lab6.sql
@@ -51,3 +51,23 @@ from People p inner join Customers c on p.pid = c.pid
               inner join Products pr on o.prodId = pr.prodId
               inner join People p2   on a.pid = p2.pid -- inner join another People table to get agent data
 where p2.homeCity = 'Teaneck';
+
+-- Question #6: Write a query to check the accuracy of the totalUSD column in the Orders table. This
+-- means calculating Orders.totalUSD from data in other tables and comparing those values to the values
+-- in Orders.totalUSD. Display all rows in Orders where Orders.totalUSD is incorrect, if any. If there
+-- are any incorrect values, explain why they are wrong. --
+select o.*, round((o.quantityOrdered * pr.priceUSD) * (1 - (c.discountPct/100)), 4) as "Actual totalUSD"
+from Orders o inner join Products pr on o.prodId = pr.prodId
+              inner join Customers c on o.custId = c.pid -- we need the customers table to get discount percent
+where o.totalUSD != (o.quantityOrdered * pr.priceUSD) * (1 - (c.discountPct/100)) -- total = quantity * price * (1 - discount %)
+
+/* These values appear as "incorrect" because of how SQL calculates decimals. In the Orders table,
+ * the totalUSD is a numeric rounded to 2 decimal places to account for the fact that US currency
+ * cannot be split further than 1 cent. However, when the total is multiplied by each customer's
+ * discount percent, some calculated totals have more than 2 decimal places. Thus, SQL reads '74871.83'
+ * and '74871.8304' as different values, making them technically 'incorrect' due to the 2-decimal place
+ * nature of currency. The only order that is truly incorrect is order number 1017, in which the totalUSD
+ * should have been rounded up to $25643.89 instead of $25643.88. I appended an "actual totalUSD" column
+ * to show the correct, unrounded calculation.
+ */
+													 
