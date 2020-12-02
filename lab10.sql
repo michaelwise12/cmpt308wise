93de5ab1f0902643cbc7f4b2f5c35e8cbaaad288
michaelwise12 <Michael.Wise1@edu>
2020-12-02 14:07:46 -0500

    Created second stored procedure for lab #10.

--- lab10.sql
+++ lab10.sql
@@ -16,13 +16,13 @@
 create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as 
 $$
 declare
-   courseNumber int    := $1;
+   courseNumber int    := $1; -- I named the variable 'courseNumber' to avoid ambiguity
    resultset REFCURSOR := $2;
 begin
    open resultset for 
       select *
       from Courses
-      where num in (select preReqNum -- query Prereq table for prereqs w/ specified course number
+      where num in (select preReqNum -- query Prereq table for prereqs of specified course number
                     from Prerequisites
                     where courseNum = courseNumber);
    return resultset;
@@ -32,3 +32,23 @@ language plpgsql;
 
 select PreReqsFor(308, 'results');
 fetch all from results;
+
+-- Question #2: Write a function IsPreReqFor(courseNumber) - Returns the courses for
+-- which the passed-in course number is an immediate prerequisite. --
+
+create or replace function IsPreReqFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   courseNumber int    := $1;
+   resultset REFCURSOR := $2;
+begin
+   open resultset for 
+      select *
+      from Courses
+      where num in (select courseNum -- query Prereq table for courses that ARE a prereq for a course
+                    from Prerequisites
+                    where preReqNum = courseNumber);
+   return resultset;
+end;
+$$ 
+language plpgsql;
